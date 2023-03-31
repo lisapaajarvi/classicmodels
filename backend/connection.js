d07e62539208afe087c3a1ff5d6d54e461152457
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2023-03-31 15:38:51 +0200

    added port

--- backend/connection.js
+++ backend/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2")
 
 connection = mysql.createConnection({
     host: process.env.HOST,
-    port: "8889",
+    port: process.env.DATABASE_PORT,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_USER,
     database: process.env.DATABASE_USER
