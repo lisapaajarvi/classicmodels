7c1e17cc276c41ed59ea5e6bab47c8fef1d1f18b
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2023-03-31 12:05:11 +0200

    endpoints for products

--- backend/app.js
+++ backend/app.js
@@ -13,4 +13,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Get all product lines
+app.get("/productlines", (req, res) => {
+    connection.connect((err) => {
+        if(err) {
+        console.log("err", err);
+        }
+        connection.query("SELECT * FROM productlines", (err, data) => {
+            if(err) {
+                console.log("err", err);
+                }
+            res.json(data)
+        })
+    })
+});
+
+// Get all products for a certain product line
+app.get("/products/:productline", (req, res) => {
+    let productline = req.params.productline;
+    connection.connect((err) => {
+        if(err) {
+        console.log("err", err);
+        }
+        connection.query(`SELECT * FROM products WHERE productLine = '${productline}'`, (err, data) => {
+            if(err) {
+                console.log("err", err);
+                }
+            res.json(data)
+        })
+    })
+});
+
 module.exports = app;
